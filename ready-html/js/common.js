e7f3b054f12c6bc522a94b11e197e58aee56c080
botirjon <garrow088@gmail.com>
2024-02-21 17:48:10 +0500

    music

--- ready-html/js/common.js
+++ ready-html/js/common.js
@@ -14,3 +14,22 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.querySelector('video').playbackRate = 2
 
 })
+
+// Audio
+
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+	audio.pause()
+}
